900057b4da4e4e5a8211680b5494d91575a20ae4
JunShen <tony88010955@gmail.com>
2023-08-15 00:28:37 +0800

    finish project

--- app.js
+++ app.js
@@ -8,26 +8,32 @@ let filteredProducts = [...products];
 const productsContainer = document.querySelector(".products-container");
 
 const displayProducts = () => {
-  productsContainer.innerHTML = filteredProducts
-    .map((product) => {
-      return `<article class="product" id=${product.id}>
-          <img
-            src=${product.image}
-            class="product-img img"
-          />
-          <footer>
+  if (filteredProducts.length < 1) {
+    // console.log(filteredProducts);
+    productsContainer.innerHTML = `<h1>Sorry, we don't have such that products</h1>`;
+    // return;
+  } else {
+    productsContainer.innerHTML = filteredProducts
+      .map((product) => {
+        return `<article class="product" id=${product.id}>
+      <img
+      src=${product.image}
+      class="product-img img"
+      />
+      <footer>
             <h5 class="product-name">${product.title}</h5>
             <span class="product-price">$${product.price}/span>
-          </footer>
-        </article>
+            </footer>
+            </article>
     `;
-    })
-    .join("");
+      })
+      .join("");
+  }
 };
 
 displayProducts();
 
-//text filters
+// Text filters
 const form = document.querySelector(".input-form");
 const searchInput = document.querySelector(".search-input");
 
@@ -39,4 +45,40 @@ form.addEventListener("keyup", () => {
   console.log(filteredProducts);
 
   displayProducts();
+});
+
+// Filter Buttons
+const companiesDOM = document.querySelector(".companies");
+
+const displayButtons = () => {
+  const buttons = [
+    "all",
+    ...new Set(products.map((product) => product.company)),
+  ];
+  console.log(buttons);
+  companiesDOM.innerHTML = buttons
+    .map((company) => {
+      // data-XXX -> we can find it from el.dataset.XXX
+      return `<button class='company-btn' data-id=${company}>${company}</button>`;
+    })
+    .join("");
+};
+
+displayButtons();
+
+companiesDOM.addEventListener("click", (event) => {
+  console.log(event.target);
+  const el = event.target;
+  if (el.classList.contains("company-btn")) {
+    if (el.dataset.id === "all") {
+      filteredProducts = [...products];
+    } else {
+      filteredProducts = products.filter((product) => {
+        return product.company === el.dataset.id;
+      });
+    }
+
+    searchInput.value = "";
+    displayProducts();
+  }
 });
