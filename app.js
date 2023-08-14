cab88ff71fe7f5849a0e4e755c9f5dd744ef3bf1
JunShen <tony88010955@gmail.com>
2023-08-14 19:41:53 +0800

    text-filters finish

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import { products } from "./products.js";
 
 // make a copy and assign to a new variable
 let filteredProducts = [...products];
-console.log(filteredProducts);
+// console.log(filteredProducts);
 
 // select .products-container
 const productsContainer = document.querySelector(".products-container");
 
 const displayProducts = () => {
-  productsContainer.innerHTML = products
+  productsContainer.innerHTML = filteredProducts
     .map((product) => {
       return `<article class="product" id=${product.id}>
           <img
@@ -26,3 +26,17 @@ const displayProducts = () => {
 };
 
 displayProducts();
+
+//text filters
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
+form.addEventListener("keyup", () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  console.log(filteredProducts);
+
+  displayProducts();
+});
